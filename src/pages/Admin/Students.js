dd608efbdcfdd5e831bd88ca580179d334adf6b1
Shoaib Ihsan <theshoaibihsan10@gmail.com>
2023-01-20 12:49:33 +0500

    completed

--- src/pages/Admin/Students.js
+++ src/pages/Admin/Students.js
@@ -175,15 +175,15 @@ import  Axios  from 'axios';
         
         {/* Update Student Model Start */}
         <div class="modal fade" id="updateStudentModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Update student</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Update student</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
 
      
                                 <div className="row g-3">
